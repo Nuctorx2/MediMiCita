8cfb92464d34c08be5fca6756e88498017fa7179
Juan Alejandro Rojas Vargas <u20231210916@usco.edu.co>
2025-05-14 17:17:00 -0500

    Initial commit

---
+++ src/main/java/co/edu/usco/medimicita/MediMiCitaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usco.medimicita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediMiCitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediMiCitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/usco/medimicita/MediMiCitaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usco.medimicita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediMiCitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
